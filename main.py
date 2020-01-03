843ec37490f005969438225f5b216ac9fd513510
Mathieu <40959576+MatRanc@users.noreply.github.com>
2020-01-03 02:44:52 -0500

    Added brand scraping, cleaned up, added comments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#https://www.canadacomputers.com/search/results_details.php?language=en&keywords=graphics+card
-#https://www.youtube.com/watch?v=XQgXKtPSzUI&feature=emb_logo
+"""
+Links:
+https://www.canadacomputers.com/search/results_details.php?language=en&keywords=graphics+card
+https://www.youtube.com/watch?v=XQgXKtPSzUI&feature=emb_logo
 
+Todo:
+Actually read normal price when on sale
+"""
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
@@ -20,6 +25,8 @@ containers = page_soup.findAll("div", {"class":"row mx-0"})
 #Use in terminal when debugging a single container
 #container = containers[1]
 
+print("\n") #Makes new line before output for neatness
+
 #Loop to read info for all products
 for container in containers:
 	#Product name
@@ -34,23 +41,31 @@ for container in containers:
 	#Product normal price
 	price_normal_container = container.findAll("span", {"class":"d-block mb-0 pq-hdr-product_price line-height"})
 
-	if price_normal_container == [] : price_normal = price_sale
+	if price_normal_container == [] : price_normal = "On sale" #Todo: make it actually read normal price
 	else: price_normal = price_normal_container[0].strong.text
-
-	print("product_name: " + product_name)
-	print("price_sale:  " + price_sale)
-	print("price_normal:  " + price_normal)
 
 	#Discount amount
 
 	#Product brand
+	brand_container = container.div.findAll("div", {"class":"pq-img-manu_logo_box"})
+	if brand_container == [] : brand = "N\A"
+	else: brand = brand_container[0].img["alt"]
 
-	""" Classes for sale price vs normal price
-	sale price class = text-danger d-block mb-0 pq-hdr-product_price line-height
-	normal price class = d-block mb-0 pq-hdr-product_price line-height
-	notice text-danger, if else statements are needed
-	"""
 
+	print("product_name: " + product_name)
+	print("price_sale:  " + price_sale)
+	print("price_normal:  " + price_normal)
+	print("brand:  " + brand)
+	print("\n") #Adds a new line to make output neater
+
+
+
+"""
+	data_list_container = container.findAll("div", {"class":"d-inline pl-0_5"})
+	data_list = data_list_container[0]
+
+	data_list.findAll("ul", {"class":"&quot"})
+"""
 
 #To get more products, triggerScroll() will be needed?
 print("product_name: " + product_name)
