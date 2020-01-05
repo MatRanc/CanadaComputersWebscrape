b8c317c7d1f45c3f95c8fc619b654070c5b662f3
Mathieu <40959576+MatRanc@users.noreply.github.com>
2020-01-05 00:48:54 -0500

    added discount amount + cleanup

--- main.py
+++ main.py
@@ -57,17 +57,20 @@ for container in containers:
 	else: price_normal = price_normal_container[0].strong.text
 
 	#Discount amount
-	
+
+	if price_sale_container == [] : discount_amount = 0
+	else: discount_amount = float(price_normal_on_sale.strip("$")) - float(price_sale.strip("$")) 
+
 	#Product brand
 	brand_container = container.div.findAll("div", {"class":"pq-img-manu_logo_box"})
 	if brand_container == [] : brand = "N\A"
 	else: brand = brand_container[0].img["alt"]
 
-
+	print("brand:  " + brand)
 	print("product_name: " + product_name)
 	print("price_sale:  " + price_sale)
 	print("price_normal:  " + price_normal)
-	print("brand:  " + brand)
+	print("discount_amount:  $" + str(discount_amount))
 	print("\n") #Adds a new line to make output neater
 
 #To get more products, triggerScroll() will be needed?
